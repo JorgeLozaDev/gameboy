15a3d492f6b3e12aa6fa37844153b5850e9f0a9f
jorge <j0rg3e93@gmail.com>
2023-10-05 13:56:00 +0200

    Se agrega: 1 video para la opción 2, 1 gif y audio para la opción 3 y se agrega la logica necesaria para que se muestren estos elementos cuando se seleccione su opción en el menu

--- src/js/myjs.js
+++ src/js/myjs.js
@@ -92,6 +92,35 @@ document.getElementById("btnA").onclick = function () {
             "<video  id='video' src='src/img/pokemon.mp4'></video>";
           // con esta funcion simulamos el click en el play del video
           document.getElementById("video").play();
+          //con la siguiente funcion controlamos cuando termina el video y cuando termine queremos que se muestre otra cosa en lugar del video
+          document
+            .getElementById("video")
+            .addEventListener("ended", function (e) {
+              pantalla[0].innerHTML =
+                "<div class='opciones'><div class='menu' id='menuOpciones'><p class='fondoOpcionMenu' data-opc='video'>Ver video</p><p data-opc='sorpresa1'>Sorpresa</p><p data-opc='sorpresa2'>Sorpresa 2</p></div><div class='instrucciones'><div><p><i class='fa fa-caret-up'></i>UP</p><p>/</p><p><i class='fa fa-caret-down'></i>DOWN</p></div><div><p>A : SELECT</p></div></div></div>";
+              // añadimos una clase para una transicion
+              pantalla[0].classList.add("fade-in");
+            });
+        } else if (
+          menu.childNodes[index].getAttribute("data-opc") == "sorpresa1"
+        ) {
+          //cargamos el video en el div correspondiente
+          pantalla[0].innerHTML =
+            "<video  id='video' src='src/img/hello_world.mp4' style='object-fit:fill;'></video>";
+          // con esta funcion simulamos el click en el play del video
+          document.getElementById("video").play();
+          //con la siguiente funcion controlamos cuando termina el video y cuando termine queremos que se muestre otra cosa en lugar del video
+          document
+            .getElementById("video")
+            .addEventListener("ended", function (e) {
+              pantalla[0].innerHTML =
+                "<div class='opciones'><div class='menu' id='menuOpciones'><p class='fondoOpcionMenu' data-opc='video'>Ver video</p><p data-opc='sorpresa1'>Sorpresa</p><p data-opc='sorpresa2'>Sorpresa 2</p></div><div class='instrucciones'><div><p><i class='fa fa-caret-up'></i>UP</p><p>/</p><p><i class='fa fa-caret-down'></i>DOWN</p></div><div><p>A : SELECT</p></div></div></div>";
+              // añadimos una clase para una transicion
+              pantalla[0].classList.add("fade-in");
+            });
+        }else{
+          pantalla[0].innerHTML ="<img src='src/img/culo.gif'><audio controls id='musica'><source src='src/img/My_Little_Pony.mp3' type='audio/mpeg'>Your browser does not support the audio element.</audio>";
+          document.getElementById("musica").play();
         }
       }
     }
