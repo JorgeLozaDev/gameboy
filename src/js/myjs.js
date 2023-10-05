8fd68b5320c9df70122d3a14db3b3a6562b822bf
jorge <j0rg3e93@gmail.com>
2023-10-05 09:44:42 +0200

    Se cambia el gif del logo por un video, se añade codigo en el js para que el video funcione y se oculte cuando debe

--- src/js/myjs.js
+++ src/js/myjs.js
@@ -7,15 +7,22 @@ document.getElementById("onBtn").onclick = function () {
   let pantalla = document.getElementsByClassName("pantallaInterior");
 
   if (this.classList.contains("encendido")) {
-    // mostramos la pantalla de carga
+    // Cuando le damos al botón de encendido, esperamos 1 sg y mostramos la pantalla de carga
     setTimeout(() => {
-      pantalla[0].innerHTML = "<img src='../src/img/loading.gif'>";
-    }, 1000);
+      //cargamos el video en el div correspondiente
+      pantalla[0].innerHTML =
+        "<video  id='video' src='src/img/onVideo.mp4'></video>";
+      // con esta funcion simulamos el click en el play del video
+      document.getElementById("video").play();
 
-    // despues de la carga ficticia limpiamos la pantalla
-    setTimeout(() => {
-      pantalla[0].innerHTML = "<div class='opciones'><p>Ver video</p> <p>Sorpresa</p> <p>Sorpresa 2</p></div>";
-    }, 2800);
+      //con la siguiente funcion controlamos cuando termina el video y cuando termine queremos que se muestre otra cosa en lugar del video
+      document.getElementById("video").addEventListener("ended", function (e) {
+        setTimeout(() => {
+          pantalla[0].innerHTML =
+            "<div class='opciones'><div class='menu'><p>Ver video</p> <p>Sorpresa</p> <p>Sorpresa 2</p></div> <div class='instrucciones'></div> </div>";
+        }, 500);
+      });
+    }, 1000);
   } else {
     console.log("apagado");
     pantalla[0].innerHTML = "";
